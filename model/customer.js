3899acceece264b0687657eebfb8196b98b39eca
shenhui <shenh1986@163.com>
2018-03-11 17:14:44 +0800

    新增model

--- model/customer.js
+++ model/customer.js
@@ -3,10 +3,12 @@ var mongoose = require('./db.js'),
     Schema = mongoose.Schema;
 
 var  CustomerSchema = new Schema({
+    customer_id:String,
     name:{ type:String, unique: true },                 //名称唯一
     catogory:String,                                    //类别
     creditRating:String,                                 //信用等级
-    invoiceInfo:String                                  //开票信息
+    invoiceInfo:String,                                  //开票信息
+    area:String
 });
 module.exports = mongoose.model('Customer',CustomerSchema);
 
--- model/product.js
+++ model/product.js
@@ -3,9 +3,13 @@ var mongoose = require('./db.js'),
     Schema = mongoose.Schema;
 
 var  ProductSchema = new Schema({
+    product_no:String,
     name:{ type:String, unique: true },
     spec:String,                    //基本规格
-    specescription:String           //详细规格说明
+    description:String,             //备注描述
+    color:String,                   //颜色
+    category:String,                //特料 普料
+    identity:Boolean                //是否确认
 
 });
 module.exports = mongoose.model('Product',ProductSchema);
