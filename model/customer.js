51fe336b040b97a82c59d981720d94bfecee13df
shenhui <shenh1986@163.com>
2018-03-11 19:26:20 +0800

    debug where query

--- model/customer.js
+++ model/customer.js
@@ -3,12 +3,17 @@ var mongoose = require('./db.js'),
     Schema = mongoose.Schema;
 
 var  CustomerSchema = new Schema({
-    customer_id:String,
+    customer_no:{ type:String, unique: true },          //编码
     name:{ type:String, unique: true },                 //名称唯一
     catogory:String,                                    //类别
     creditRating:String,                                 //信用等级
     invoiceInfo:String,                                  //开票信息
     area:String
 });
+
+
+
+
+
 module.exports = mongoose.model('Customer',CustomerSchema);
 
--- test/customer.js
+++ test/customer.js
@@ -1,14 +1,14 @@
 var CustomerModel = require("../model/customer.js");
 
-
-var json = [{"name":"百度科技", "catogory":"A", "creditRating":"A", "invoiceInfo":"123"},
-    {"name":"马化腾腾讯", "catogory":"A", "creditRating":"A", "invoiceInfo":"123"},
-    {"name":"马云阿里", "catogory":"123", "creditRating":"123", "invoiceInfo":"123"},
-    {"name":"新浪科技","catogory":"123", "creditRating":"123", "invoiceInfo":"123"},
-    {"name":"淘宝科技", "catogory":"123", "creditRating":"123", "invoiceInfo":"123"},
-    {"name":"科大讯飞","catogory":"123", "creditRating":"123", "invoiceInfo":"123"},
-    {"name":"快播科技","catogory":"123", "creditRating":"123", "invoiceInfo":"123"},
-    {"name":"上汽雪佛兰","catogory":"123", "creditRating":"123", "invoiceInfo":"123"}
+/*
+var json = [{"customer_no":"10001","name":"百度科技", "catogory":"A", "creditRating":"A", "invoiceInfo":"123"},
+    {"customer_no":"10002","name":"马化腾腾讯", "catogory":"A", "creditRating":"A", "invoiceInfo":"123"},
+    {"customer_no":"10003","name":"马云阿里", "catogory":"123", "creditRating":"123", "invoiceInfo":"123"},
+    {"customer_no":"10004","name":"新浪科技","catogory":"123", "creditRating":"123", "invoiceInfo":"123"},
+    {"customer_no":"10005","name":"淘宝科技", "catogory":"123", "creditRating":"123", "invoiceInfo":"123"},
+    {"customer_no":"10006","name":"科大讯飞","catogory":"123", "creditRating":"123", "invoiceInfo":"123"},
+    {"customer_no":"10007","name":"快播科技","catogory":"123", "creditRating":"123", "invoiceInfo":"123"},
+    {"customer_no":"10008","name":"上汽雪佛兰","catogory":"123", "creditRating":"123", "invoiceInfo":"123"}
     ];
 
 for(var i in json){
@@ -22,6 +22,16 @@ for(var i in json){
         }
     });
 }
+*/
+
+var Query = CustomerModel.find({});
+CustomerModel.find().$where(function () {
+    return this.customer_no =='10001' ||this.customer_no =='10002' ;
+}).exec(function(err,data){
+   if(!err){
+       console.log("data size:"+data.length);
+   }
+});
 
 
 
